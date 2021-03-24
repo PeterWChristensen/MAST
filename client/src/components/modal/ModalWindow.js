75a84e0ce1426863c900af6a1f1c17bf33cda4d4
Peter Christensen <peter.christensen@stonybrook.edu>
2021-03-24 19:04:18 -0400

    fixed issues with importing grades

--- client/src/components/modal/ModalWindow.js
+++ client/src/components/modal/ModalWindow.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CSVReader from "react-csv-reader";
 import CourseOfferingsService from "../../services/courseOfferings.service";
+import GradeService from "../../services/importGrade.service";
 
 const papaparseOptions = {
   header: true,
