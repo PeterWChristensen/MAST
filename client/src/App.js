28d99dcec46fba71b8dd8184d801d13497dd9d06
Haley <haley.pace@stonybrook.edu>
2021-04-14 19:56:14 -0400

    Fixed MS View/edit GUI and added links to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ import AddStudentScreen from './components/add_student/AddStudentScreen.js';
 // import Navbar from './components/navbar/Navbar.js';
 import Profile from "./components/Student_Prof/ProfileScreen.js";
 import HomeScreen from "./components/home_screen/Home.component";
-import MSViewStudent from "./components/ms_student_screen/TestViewStu.js";
-import MSEditStudent from "./components/ms_student_screen/TestViewStu.js";
+import MSViewStudentScreen from "./components/ms_student_screen/MSViewStudentScreen.js";
+import MSEditStudentScreen from "./components/ms_student_screen/MSEditStudentScreen.js";
 import ViewStudentScreen from './components/student_screens/ViewStudentScreen.js';
 import EditStudentScreen from './components/student_screens/EditStudentScreen.js';
 import AuthService from './services/auth.service';
@@ -119,8 +119,8 @@ class App extends Component{
                 <Route exact path="/addStudent" component={AddStudentScreen} />
                 <Route exact path="/viewStudent" component={ViewStudentScreen} />
                 <Route exact path="/editStudent" component={EditStudentScreen} />
-                <Route exact path="/student" component={MSViewStudent} /> 
-                <Route exact path="/student/edit" component={MSEditStudent} /> 
+                <Route exact path="/student" component={MSViewStudentScreen} /> 
+                <Route exact path="/student/edit" component={MSEditStudentScreen} /> 
               </Switch>
             </div>
        </div>
